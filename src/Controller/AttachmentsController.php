2c9c494e28bb3f349c5a680ee993bf8ecf2318df
busgurlu <burak@uskur.com.tr>
2020-11-01 00:36:26 +0300

    stream file

--- src/Controller/AttachmentsController.php
+++ src/Controller/AttachmentsController.php
@@ -294,4 +294,87 @@ class AttachmentsController extends AppController
         $this->set('_serialize', ['attachment']);
     }
 
+    /**
+     * @param $id
+     * @param null $name
+     * @throws \Exception
+     * https://cakephp.blog/stream-video-with-cakephp-pt-1/
+     */
+    public function stream($id, $name = null)
+    {
+        $this->buffer = 102400;
+        $attachment = $this->Attachments->get($id);
+        if (!file_exists($attachment->path)) {
+            throw new \Exception("File {$attachment->path} cannot be read.");
+        }
+        if (!($this->stream = fopen($attachment->path, 'rb'))) {
+            throw new \Exception("Stream could not be opened.");
+        }
+
+        ob_get_clean();
+        header("Content-Type: video/mp4");
+        header("Cache-Control: max-age=311040000, public");
+        header("Expires: ".gmdate('D, d M Y H:i:s', time()+311040000) . ' GMT');
+        header("Last-Modified: ".gmdate('D, d M Y H:i:s', @filemtime($attachment->path)) . ' GMT' );
+        $this->start = 0;
+        $this->size  = filesize($attachment->path);
+        $this->end   = $this->size - 1;
+
+        header("Accept-Ranges: 0-".$this->end);
+        //set header
+        if (isset($_SERVER['HTTP_RANGE'])) {
+
+            $c_start = $this->start;
+            $c_end = $this->end;
+
+            list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+            if (strpos($range, ',') !== false) {
+                header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                header("Content-Range: bytes $this->start-$this->end/$this->size");
+                exit;
+            }
+            if ($range == '-') {
+                $c_start = $this->size - substr($range, 1);
+            }else{
+                $range = explode('-', $range);
+                $c_start = $range[0];
+                $c_end = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $c_end;
+            }
+            $c_end = ($c_end > $this->end) ? $this->end : $c_end;
+            if ($c_start > $c_end || $c_start > $this->size - 1 || $c_end >= $this->size) {
+                header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                header("Content-Range: bytes $this->start-$this->end/$this->size");
+                exit;
+            }
+            $this->start = $c_start;
+            $this->end = $c_end;
+            $length = $this->end - $this->start + 1;
+            fseek($this->stream, $this->start);
+            header('HTTP/1.1 206 Partial Content');
+            header("Content-Length: ".$length);
+            header("Content-Range: bytes $this->start-$this->end/".$this->size);
+        }
+        else
+        {
+            header("Content-Length: ".$this->size);
+        }
+        //stream
+        $i = $this->start;
+        set_time_limit(0);
+        while(!feof($this->stream) && $i <= $this->end) {
+            $bytesToRead = $this->buffer;
+            if(($i+$bytesToRead) > $this->end) {
+                $bytesToRead = $this->end - $i + 1;
+            }
+            $data = @stream_get_contents($this->stream, $bytesToRead, intval($i));
+            echo $data;
+            flush();
+            $i += $bytesToRead;
+        }
+
+        //close
+        fclose($this->stream);
+        exit;
+    }
+
 }
